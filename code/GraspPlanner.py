dccb6d2ea51239f31c12c6d19aeac2a7a5e056e0
David Robinson <dvsrobinson@gmail.com>
2018-04-19 05:51:25 -0400

    cvt to homogenous, trying a few things

--- code/GraspPlanner.py
+++ code/GraspPlanner.py
@@ -78,20 +78,27 @@ class GraspPlanner(object):
 
             # get gaussian kernels for pose sampling around grasp tf
             densityfn, samplerfn, bounds = self.irmodel.computeBaseDistribution(Tgrasp)
-            poses, jointstate = samplerfn(100)
+            poses, jointstate = samplerfn(1000) # we can tune # samples
 
             # Store Initial Poses
-            initial_pose = self.robot.GetTransform()
             initial_config = self.robot.GetActiveDOFValues()
+            initial_pose = self.robot.GetTransform()
 
             # Iterate through new poses to find a valid configuration
-            count = 0
-            for base_pose in poses:
-                print "sample #", count
-                count += 1
+            minDist = 1e10
+            for pose in poses:
 
                 # Set to pose and check for collision
-                self.robot.SetTransform(base_pose)
+
+                continuous_pose = copy.deepcopy([pose[4], pose[5], angle[2]])
+                node = self.base_planner.planning_env.discrete_env.ConfigurationToNodeId(continuous_pose)
+                discrete_pose = self.base_planner.planning_env.discrete_env.NodeIdToConfiguration(node)
+                
+                pdb.set_trace()
+
+                self.robot.SetTransform(pose)
+
+
                 obstacles = self.robot.GetEnv().GetBodies()
                 if self.robot.GetEnv().CheckCollision(self.robot, obstacles[1]) == False:
 
@@ -102,31 +109,68 @@ class GraspPlanner(object):
 
                     if not grasp_config is None: # check validity
 
-                        self.robot.SetTransform(base_pose)
                         self.robot.SetActiveDOFValues(grasp_config)
+                        self.robot.SetTransform(pose)
+                        base_pose = self.robot.GetTransform() # we get a homogeneous transform this way
 
+                        # compare dist to find min!
+                        dist = self.basePoseDistance(initial_pose, base_pose)
+                        if dist < minDist:
+                            base_pose_ret = base_pose
+                            grasp_config_ret = grasp_config
+                            minDist = dist
+
+
+                        self.robot.SetActiveDOFValues(grasp_config_ret)
+                        self.robot.SetTransform(base_pose_ret)
                         raw_input("Collision-free config found (enter)...")
 
                         self.robot.SetTransform(initial_pose)
                         self.robot.SetActiveDOFValues(initial_config)
 
-                        return self.convertPose(base_pose), grasp_config
+                        return self.convertPose(base_pose_ret), grasp_config_ret, base_pose_ret
+
+
+    def basePoseDistance(self, initial_pose, final_pose):
+        return math.sqrt( (initial_pose[0][3] - final_pose[0][3])**2 + (initial_pose[1][3] - final_pose[1][3])**2 )
+
 
     def convertPose(self, H):
-
         # homogeneous to x,y,heading
-        angle = openravepy.axisAngleFromQuat(H)
-        pose = [H[4], H[5], angle[2]]
-        
+        x,y,z = self.mat2euler(H)
+        pose = [H[0][3], H[1][3], z]
         print "Homogeneous:", H
         print "X,Y,Heading:", pose
-
         return pose
+
+
+    # https://afni.nimh.nih.gov/pub/dist/src/pkundu/meica.libs/nibabel/eulerangles.py
+    def mat2euler(self, M, cy_thresh=None):
+        M = M[0:3,0:3]
+        M = np.asarray(M)
+        if cy_thresh is None:
+            try:
+                cy_thresh = np.finfo(M.dtype).eps * 4
+            except ValueError:
+                cy_thresh = _FLOAT_EPS_4
+        r11, r12, r13, r21, r22, r23, r31, r32, r33 = M.flat
+        # cy: sqrt((cos(y)*cos(z))**2 + (cos(x)*cos(y))**2)
+        cy = math.sqrt(r33*r33 + r23*r23)
+        if cy > cy_thresh: # cos(y) not close to zero, standard form
+            z = math.atan2(-r12,  r11) # atan2(cos(y)*sin(z), cos(y)*cos(z))
+            y = math.atan2(r13,  cy) # atan2(sin(y), cy)
+            x = math.atan2(-r23, r33) # atan2(cos(y)*sin(x), cos(x)*cos(y))
+        else: # cos(y) (close to) zero, so x -> 0.0 (see above)
+            # so r21 -> sin(z), r22 -> cos(z) and
+            z = math.atan2(r21,  r22)
+            y = math.atan2(r13,  cy) # atan2(sin(y), cy)
+            x = 0.0
+        return x, y, z
 
     def PlanToGrasp(self, obj):
 
         # Next select a pose for the base and an associated ik for the arm
-        base_pose, grasp_config = self.GetBasePoseForObjectGrasp(obj)
+        base_pose, grasp_config, test = self.GetBasePoseForObjectGrasp(obj)
 
         if base_pose is None or grasp_config is None:
             print 'Failed to find solution'
@@ -139,6 +183,10 @@ class GraspPlanner(object):
 
         print 'Executing base trajectory'
         self.base_planner.planning_env.herb.ExecuteTrajectory(base_traj)
+
+        raw_input("...")
+        self.robot.SetTransform(test)
+        raw_input("...")
 
         # Now plan the arm to the grasp configuration
         start_config = np.array(self.arm_planner.planning_env.herb.GetCurrentConfiguration())
